6f3145bf730c1b909843dfe8d3edc86fa587c74a
mertcanekiz <ekiz.mertcan@gmail.com>
2019-11-20 01:15:34 +0300

    Remove unnecessary code

--- src/main.c
+++ src/main.c
@@ -5,8 +5,6 @@ int main(int argc, char** argv)
     printf("Melkor Engine v0.1\n");
     App *app = create_app("melkor engine v0.1",
                           640, 480, false);
-//    set_callback(app->window, KEY_CALLBACK, &key_callback);
-//    set_callback(app->window, WINDOW_CLOSE_CALLBACK, &window_close_callback);
     app->is_running = true;
     while (app->is_running) {
         clear_window();
