8bd473b6a3ec8a489bbce9bc13c6e11fa0e16cb1
mertcanekiz <ekiz.mertcan@gmail.com>
2019-11-20 12:17:30 +0300

    Add subdirectories

---
+++ src/melkor/core/app.h
@@ -0,0 +1,17 @@
+#ifndef APP_H
+#define APP_H
+
+#include "window.h"
+#include "event/callbacks.h"
+#include "event/event.h"
+
+typedef struct App {
+    Window *window;
+    bool is_running;
+} App;
+
+/* Functions */
+App* create_app(const char* title, uint16_t width, uint16_t height, bool fullscreen);
+void update_app(App *app);
+
+#endif // APP_H
